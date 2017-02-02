34468587271425bad1c55a216f7d0e34976a1acf
Nulifier <nulifier@gmail.com>
2017-02-02 00:02:20 -0700

    Updated the demo

--- demo/app.js
+++ demo/app.js
@@ -1,1 +1,12 @@
-m.mount(document.getElementById('select-demo'), mithrilLib.select);
+m.mount(document.getElementById('select-demo'), mithrilLib.Select);
+
+
+var TooltipDemo = {
+	view: function() {
+		return m(mithrilLib.Tooltip, [
+			m('span', 'Tooltip')
+		]);
+	}
+};
+
+//m.mount(document.getElementById('tooltip-demo'), TooltipDemo);
