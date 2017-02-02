f60f1f95b274719ee0ec0fb3df25a4a82a79fce3
Nulifier <nulifier@gmail.com>
2017-02-02 00:01:33 -0700

    Worked on the select control

--- src/select.js
+++ src/select.js
@@ -1,5 +1,14 @@
 import m		from 'mithril';
 
 export function view(vnode) {
-	return m('div', 'Test');
+	return m('.mlib-select', [
+		m('.mlib-select-control', [
+			m('.mlib-select-wrapper', [
+				m('.mlib-select-placeholder', 'Select...')
+			]),
+			m('.mlib-select-arrow-zone', [
+				m('span.mlib-select-arrow')
+			])
+		])
+	]);
 }
